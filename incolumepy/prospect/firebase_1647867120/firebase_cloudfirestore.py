7c2107e5fcd3095c9f49b12defe798f0f8a80c3d
Britodfbr <britodfbr@gmail.com>
2022-03-22 08:58:37 -0300

    WIP

--- incolumepy/prospect/firebase_1647867120/firebase_cloudfirestore.py
+++ incolumepy/prospect/firebase_1647867120/firebase_cloudfirestore.py
@@ -2,3 +2,15 @@
 # -*- coding: utf-8 -*-
 __author__ = "@britodfbr"  # pragma: no cover
 '/atos/alvara/1888/A0001'
+
+
+def implicit():
+    from google.cloud import storage
+
+    # If you don't specify credentials when constructing the client, the
+    # client library will look for credentials in the environment.
+    storage_client = storage.Client()
+
+    # Make an authenticated API request
+    buckets = list(storage_client.list_buckets())
+    print(buckets)
