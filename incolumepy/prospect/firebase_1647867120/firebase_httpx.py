b11bf2fc3c829e78ee9aa12502671d1f5a802657
Britodfbr <britodfbr@gmail.com>
2022-03-23 08:28:52 -0300

    style: Lint corrections

--- incolumepy/prospect/firebase_1647867120/firebase_httpx.py
+++ incolumepy/prospect/firebase_1647867120/firebase_httpx.py
@@ -1,4 +1,5 @@
-# !/usr/bin/env python
+"""Firebase Realtime."""
+
 # -*- coding: utf-8 -*-
 import json
 from datetime import datetime
@@ -9,90 +10,111 @@ import httpx
 __author__ = "@britodfbr"  # pragma: no cover
 
 
-url = 'https://incolumepy-firebase-prospect-default-rtdb.asia-southeast1.firebasedatabase.app/{}'
+URL = (
+    "https://incolumepy-firebase-"
+    "prospect-default-rtdb.asia-southeast1.firebasedatabase.app/{}"
+)
 
 
 ato_fake = {
-    'epigrafe': '', 'content': 'texto ato',
-    'num': 0, 'classify': 'dec',
+    "epigrafe": "",
+    "content": "texto ato",
+    "num": 0,
+    "classify": "dec",
 }
 
 
 def add_data(num):
     """Adicionar dados com post."""
-    ato_fake.update({'num': num, 'classify': 'dec'})
-    req = httpx.post(url.format(f'/decreto/.json'), data=json.dumps(ato_fake))
+    ato_fake.update({"num": num, "classify": "dec"})
+    req = httpx.post(
+        URL.format("/decreto/.json"),
+        data=json.dumps(ato_fake),  # type: ignore
+    )
     print(req, req.text)
 
 
 def add_key_data(num, tipo=None, classify=None, dou=None, date=None):
     """Adicionar dados com put."""
-    tipo = tipo or 'decreto'
-    classify = classify or 'dec'
-    dou = dou or 'DOU p.02 http://in.gov.br'
-    date = date or datetime.now().strftime('%F %T')
+    tipo = tipo or "decreto"
+    classify = classify or "dec"
+    dou = dou or "DOU p.02 http://in.gov.br"
+    date = date or datetime.now().strftime("%F %T")
     ato_fake.update(
         {
-            'dou': dou,
-            'date': date,
-            'num': num,
-            'epigrafe': f'{tipo} nº {num} de {datetime.now().strftime("%d de %B de %Y")}',
-            'classify': classify
+            "dou": dou,
+            "date": date,
+            "num": num,
+            "epigrafe": f"{tipo} nº {num} "
+            f'de {datetime.now().strftime("%d de %B de %Y")}',
+            "classify": classify,
         }
     )
-    req = httpx.put(url.format(f'/{tipo}/{tipo[0].upper()}{num:0>4}.json'), data=json.dumps(ato_fake))
+    req = httpx.put(
+        URL.format(f"/{tipo}/{tipo[0].upper()}{num:0>4}.json"),
+        data=json.dumps(ato_fake),  # type: ignore
+    )
     print(req, req.text)
 
 
 def change_data():
-    ato_fake.update({'classify': 'DIM'})
-    req = httpx.patch(url.format('/decreto/-MyhqoBUmGmmyjXaB3d6.json'), data=json.dumps(ato_fake))
+    """Change data."""
+    ato_fake.update({"classify": "DIM"})
+    req = httpx.patch(
+        URL.format("/decreto/-MyhqoBUmGmmyjXaB3d6.json"),
+        data=json.dumps(ato_fake),  # type: ignore
+    )
     print(req, req.text)
 
 
 def get_data():
-    req = httpx.get(url.format('.json'))
+    """Get data."""
+    req = httpx.get(URL.format(".json"))
     print(req)
     print(req.content)
     pprint(req.json())
 
 
 def get_lei_data():
-    req = httpx.get(url.format('/lei.json'))
+    """Get lei."""
+    req = httpx.get(URL.format("/lei.json"))
     pprint(req.json())
 
 
 def delete_lei():
-    req = httpx.delete(url.format('/lei/L0010.json'))
+    """Delete lei."""
+    req = httpx.delete(URL.format("/lei/L0010.json"))
     print(req, req.text)
 
 
 def delete_dec():
-    req = httpx.get(url.format('/decreto.json'))
+    """Delete decreto."""
+    req = httpx.get(URL.format("/decreto.json"))
     atos = req.json()
     for ato in atos:
         # print(ato)
         # print(atos[ato])
-        if atos[ato]['classify'] == 'dec':
-            r = httpx.delete(url.format(f'/decreto/{ato}.json'))
+        if atos[ato]["classify"] == "dec":
+            r = httpx.delete(URL.format(f"/decreto/{ato}.json"))
             print(r, r.text)
 
 
 def delete_dec_melhorado():
-    req = httpx.get(url.format('/decreto.json'))
+    """Delete decreto enhacement."""
+    req = httpx.get(URL.format("/decreto.json"))
     for key, ato in req.json().items():
         print(key, ato)
-        if ato['num'] == 0:
-            r = httpx.delete(url.format(f'decreto/{key}.json'))
+        if ato["num"] == 0:
+            r = httpx.delete(URL.format(f"decreto/{key}.json"))
             print(r, r.text)
 
 
 def run():
+    """Run it."""
     for n in range(11):
-        pass
         add_data(n)
-        add_key_data(n, tipo='decreto', classify='DCM')
-        add_key_data(n, tipo='lei', classify='LIM')
+        add_key_data(n, tipo="decreto", classify="DCM")
+        add_key_data(n, tipo="lei", classify="LIM")
     change_data()
     get_data()
     get_lei_data()
@@ -101,5 +123,5 @@ def run():
     delete_dec_melhorado()
 
 
-if __name__ == '__main__':  # pragma: no cover
+if __name__ == "__main__":  # pragma: no cover
     run()
